afe6fc24fcf180f1a069af501a724cd6bc9b34b4
Nobert-Muma <mumanobert17@gmail.com>
2024-06-27 01:46:00 +0300

    Scope in JavaScript

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+
+const leastFavoriteCustomer='Flur';
+var customerName='bob';
+var bestCustomer='not bob';
+function upperCaseCustomerName() {
+    console.log(customerName);
+    console.log(customerName.toUpperCase());    
+    //console.log(customerName);
+}
+upperCaseCustomerName();
+function setBestCustomer() {
+    //var bestCustomer='not bob';
+    console.log(bestCustomer);
+}
+setBestCustomer();
+function overwriteBestCustomer() {
+    bestCustomer='maybe bob';
+    return bestCustomer;
+}
+overwriteBestCustomer();
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer="nOBERT";
+    console.log(leastFavoriteCustomer);
+}
+changeLeastFavoriteCustomer();
